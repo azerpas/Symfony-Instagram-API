68b1a12814fed58595c499eae54b1634a1d39603
Anthony Manikhouth <Anthony@macbook-pro-anthony.home>
2019-03-07 16:32:17 +0100

    Auth is now functionnal

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -84,9 +84,9 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
         if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
             return new RedirectResponse($targetPath);
         }
-
+        return new RedirectResponse($this->urlGenerator->generate('inst_home'));
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl()
