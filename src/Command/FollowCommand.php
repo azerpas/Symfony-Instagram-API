cbdc221a76e63772e3390939c2a2d837d7c6e239
NareshDRoger <nareshrogeraly@gmail.com>
2019-03-12 16:57:57 +0100

    FollowCommand : a command to follow someone by his Id

---
+++ src/Command/FollowCommand.php
@@ -0,0 +1,71 @@
+<?php
+
+// src/Command/FollowCommand.php
+namespace App\Command;
+
+use InstagramAPI\Response\Model\FriendshipStatus;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Input\ArrayInput;
+
+class FollowCommand extends Command
+{
+    // the name of the command (the part after "bin/console")
+    protected static $defaultName = 'app:follow';
+
+    protected function configure()
+    {
+        $this 
+        //Add respectively the username and password of the user and the userId to follow
+        ->addArgument('username', InputArgument::REQUIRED, 'My username')
+        ->addArgument('password', InputArgument::REQUIRED, 'My password')
+        ->addArgument('userId', InputArgument::REQUIRED, 'The Id of the account to follow')
+    ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        /*
+        $command = $this->getApplication()->find('insta:instance');
+
+        $arguments = [
+            'command'  => 'insta:instance',
+            'username' => $input->getArgument('username') ,
+            'password' => $input->getArgument('password') ,
+        ];
+
+        $instanceInput = new ArrayInput($arguments);
+        $ig = $command->run($instanceInput, $output);
+        */
+
+        /////// CONFIG ///////
+        $debug          = false;
+        $truncatedDebug = false;
+        //////////////////////
+        $username = $input->getArgument('username');
+        $password = $input->getArgument('password');
+        $ig = new \InstagramAPI\Instagram($debug, $truncatedDebug);
+        $toFollowId = $input->getArgument('userId');
+        try {
+            $ig->login($username, $password);
+            $ig->people->follow($toFollowId);
+            $output->write('Follow effectué!');
+        } catch (\Exception $e) {
+            throw new \Exception('Something went wrong: ' . $e->getMessage());
+        }
+
+        /*$toFollowId = $input->getArgument('userId');
+        try {
+            $ig->people->follow($toFollowId);
+            $output->write('Succes!');
+        }
+        catch (\Exception $e) {
+            throw new \Exception('Something went wrong: ' . $e->getMessage());
+      }*/
+    }
+
+}
